7e47a304d355dc407c0331834e15f7105eb3d147
anorlov <eaglecrazy@mail.ru>
2021-03-03 19:06:47 +0300

    update PSR7

--- public/index.php
+++ public/index.php
@@ -1,35 +1,52 @@
 <?php
 
-use Framework\Http\Request;
+use Framework\Http\RequestFactory;
+use Framework\Http\Response;
 
 chdir(dirname(__DIR__));
-require'vendor/autoload.php';
+require 'vendor/autoload.php';
 
 ### Initialization
 session_start();
-$request = (new Request())
-    ->withQueryParams($_GET)
-    ->withParsedBody($_POST);
-
+$request = RequestFactory::fromGlobals();
 
 ### Action
 
-function getLang(array $get, array $cookie, array $session, array $server, $default)
-{
-    return
-        !empty($get['lang']) ? $get['lang'] :
-            (
-            !empty($cookie['lang']) ? $cookie['lang'] :
-                (
-                !empty($session['lang']) ? $session['lang'] :
-                    (
-                    !empty($server['HTTP_ACCEPT_LANGUAGE']) ? substr($server['HTTP_ACCEPT_LANGUAGE'], 0, 2) : $default
-                    )
-                )
-            );
-}
+//function getLang(array $get, array $cookie, array $session, array $server, $default)
+//{
+//    return
+//        !empty($get['lang']) ? $get['lang'] :
+//            (
+//            !empty($cookie['lang']) ? $cookie['lang'] :
+//                (
+//                !empty($session['lang']) ? $session['lang'] :
+//                    (
+//                    !empty($server['HTTP_ACCEPT_LANGUAGE']) ? substr($server['HTTP_ACCEPT_LANGUAGE'], 0, 2) : $default
+//                    )
+//                )
+//            );
+//}
+//$name = $request->getQueryParams()['name'] ?? 'Guest';
+//$lang = getLang($_GET, $_COOKIE, $_SESSION, $_SERVER, 'en');
 
-$name = $request->getQueryParams()['name'] ?? 'Guest';
-$lang = getLang($_GET, $_COOKIE, $_SESSION, $_SERVER, 'en');
-header('X-Developer: Eagle');
-echo $name . ' ' . $lang;
+
+
+$response = (new Response('response-body'))
+    ->withHeader('X-Developer', 'eagle');
+
+###Sending
+header('HTTP/1.0' . ' ' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
+
+
+foreach ($response->getHeaders() as $name => $value) {
+    header($name . ':' . $value);
+}
+echo $response->getBody();
+
+
+ЗАКОНЧИЛ НА 1-43
+
+
+
+
+
