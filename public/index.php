33597c6b7b818f6fa7b88c4cac1c3f00657a33ef
eaglezzzzz <eaglecrazy@mail.ru>
2021-03-10 22:37:47 +0300

    update PSR7

--- public/index.php
+++ public/index.php
@@ -3,19 +3,23 @@
 use App\Http\Action\AboutAction;
 use App\Http\Action\Blog\IndexAction;
 use App\Http\Action\Blog\ShowAction;
+use App\Http\Action\CabinetAction;
 use App\Http\Action\HelloAction;
+use App\Http\Middleware\BasicAuthMiddleware;
 use Aura\Router\RouterContainer;
 use Framework\Http\ActionResolver;
-use Framework\Http\Router\Exception\RequestNotMatchedException;
 use Framework\Http\Router\AuraRouterAdapter;
+use Framework\Http\Router\Exception\RequestNotMatchedException;
+use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\HtmlResponse;
-use Zend\Diactoros\Response\JsonResponse;
 use Zend\Diactoros\ServerRequestFactory;
 use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 require 'helpers.php';
+
+$params = ['users' => ['admin' => 'psword']];
 
 ### Initialization
 
@@ -27,6 +31,22 @@ $aura = new RouterContainer();
 $routes = $aura->getMap();
 
 $routes->get('home', '/', HelloAction::class);
+$routes->get(
+    'cabinet',
+    '/cabinet',
+    function(ServerRequestInterface $request) use ($params){
+
+        $auth = new BasicAuthMiddleware($params['users']);
+        $cabinet = new CabinetAction();
+
+        return $auth($request, function (ServerRequestInterface $request) use ($cabinet){
+            return $cabinet($request);
+        });
+
+    }
+);
+
+//    new BasicAuthMiddleware($params['users']));
 $routes->get('about', '/about', AboutAction::class);
 $routes->get('blog', '/blog', IndexAction::class);
 $routes->get('blog_show', '/blog/{id}', ShowAction::class)->tokens(['id' => '\d+']);
