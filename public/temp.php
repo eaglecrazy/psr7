2a23b51e86d97cf14a56b2c6a57d2255620fd923
anorlov <eaglecrazy@mail.ru>
2021-04-09 18:51:32 +0300

    update PSR7

--- public/temp.php
+++ public/temp.php
@@ -1,8 +1,9 @@
 <?php
 
 use Framework\Http\Application;
-use Framework\Http\Template\Php\Extension\RouteExtension;
-use Framework\Http\Template\Php\PhpRenderer;
+use Framework\Http\Template\TemplateRenderer;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
@@ -15,7 +16,32 @@ $app = $container->get(Application::class);
 require 'config/pipeline.php';
 require 'config/routes.php';
 
-$renderer = new PhpRenderer('templates');
-$renderer->addExtension($container->get(RouteExtension::class));
+class TwigRenderer implements TemplateRenderer{
+
+    private Environment $twig;
+    private string      $extension;
+
+    public function __construct(Environment $twig, string $extension)
+    {
+        $this->twig = $twig;
+        $this->extension = $extension;
+    }
+
+    public function render(string $name, $params = []): string
+    {
+        return $this->twig->render($name . $this->extension, $params);
+    }
+}
+
+$loader = new FilesystemLoader();
+$loader->addPath('templates');
+
+$twig = new Environment($loader, [
+    'cache'=> 'var/cache/twig',
+    'auto_reload' => $debug,
+    'debug' => $debug,
+]);
+
+$renderer = new TwigRenderer($twig, '.html.twig');
 $html = $renderer->render('app/hello');
 echo $html;
